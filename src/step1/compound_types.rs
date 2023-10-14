0f12ffd9682c9d092fdc9eebebe66e64ec52288c
Vadym Volin <vadim.volin1@gmail.com>
2023-10-14 23:40:37 +0300

    references/compound types commit

---
+++ src/step1/compound_types.rs
@@ -0,0 +1,16 @@
+/*
+Compound Types
+Types	Literals
+Arrays	[T; N]	    [20, 30, 40],       [0; 3]
+Tuples	(),         (T,),               (T1, T2), …	(),     ('x',),     ('x', 1.2)
+*/
+
+fn main() {
+    let mut a: [i8; 10] = [42; 10];
+    a[5] = 0;
+    println!("a: {:?}", a);
+
+    let t: (i8, bool) = (7, true);
+    println!("t.0: {}", t.0);
+    println!("t.1: {}", t.1);
+}
